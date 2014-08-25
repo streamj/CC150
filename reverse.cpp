6e24eb8969180efcefa744536303800706d21a79
root <root@january.localdomain>
2014-08-25 21:36:14 +0800

    reverse C style string

---
+++ reverse.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void reverse(char str[])
+{
+    int i = 0, j = 0;
+    char tmp;
+    if (str)
+    {
+        while (str[i] != '\0')
+        {
+            ++i;
+        }
+        --i;
+        while (j < i)
+        {
+            tmp = str[i];
+            str[i] = str[j];
+            str[j] = tmp;
+            i--;
+            j++;
+        }
+    }
+}
+
+int main()
+{
+    char str[] = "abcdefghf";
+    printf("%s\n", str);
+    reverse(str);
+    printf("%s\n", str);
+    return 0;
+}
