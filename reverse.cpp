0129b9056cdf6646601cfc18fcfab0f58f357bcf
streamj <mystream@vip.qq.com>
2014-09-19 14:19:32 +0800

    Update reverse.cpp

--- reverse.cpp
+++ reverse.cpp
@@ -1,35 +1,57 @@
 // to show it's a C-style string so use the '\0'
-
 #include <stdio.h>
+#include <string.h>
 
 void reverse(char str[])
+
 {
-    if(sizeof(str) == 0) return;
+    if (strlen(str) == 0)
+    {
+        printf("it's empty\n");
+        return;
+    }
+
+    if (strlen(str) < 2)
+    {
+        printf("it's just one char\n");
+        return;
+    }
+
     int i = 0, j = 0;
     char tmp;
-    if (str)
+
+    while (str[i] != '\0')
     {
-        while (str[i] != '\0')
-        {
-            ++i;
-        }
-        --i;
-        while (j < i)
-        {
-            tmp = str[i];
-            str[i] = str[j];
-            str[j] = tmp;
-            i--;
-            j++;
-        }
+        ++i;
+    }
+    --i;
+    while (j < i)
+    {
+        tmp = str[i];
+        str[i] = str[j];
+        str[j] = tmp;
+        i--;
+        j++;
     }
 }
 
 int main()
 {
     char str[] = "abcdefghf";
+    char str1[] = "";
+    char str2[] = "a";
+
     printf("%s\n", str);
     reverse(str);
     printf("%s\n", str);
+
+    printf("%s\n", str1);
+    reverse(str1);
+    printf("%s\n", str1);
+
+    printf("%s\n", str2);
+    reverse(str2);
+    printf("%s\n", str2);
+    
     return 0;
 }
